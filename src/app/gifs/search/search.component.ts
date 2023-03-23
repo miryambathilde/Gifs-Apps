82dad34dd6dd4d6fb9f671a40843927eb13f2476
Miryam Bathilde <mbathilde@gmail.com>
2023-03-23 12:12:31 +0100

    viewchild input search

--- src/app/gifs/search/search.component.ts
+++ src/app/gifs/search/search.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: [ './search.component.scss' ]
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('txtSearch') txtSearch!: ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {
+  constructor () { }
+
+  ngOnInit (): void {
+  }
+
+  search () {
+    /*  console.log('search'); */
+    /* console.log(search); */
+    /* console.log(this.txtSearch) */
+    const value = this.txtSearch.nativeElement.value;
+    console.log(value);
+    this.txtSearch.nativeElement.value = '';
   }
 
 }
