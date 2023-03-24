0425dd416cb34775fa6b419cac038f3225a2cca6
Miryam Bathilde <mbathilde@gmail.com>
2023-03-24 11:23:00 +0100

    interface for result response added

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class GifsService {
   private apiKey: string = 'pkvDYJ4yws0SDc8yuSEFQ4YTm5LjprOw';
   private _history: string[] = [];
 
-  public results: any[] = [];
+  public results: Gif[] = [];
 
   constructor (private http: HttpClient) { }
 
@@ -28,8 +29,8 @@ export class GifsService {
 
     // https://api.giphy.com/v1/gifs/search?api_key=pkvDYJ4yws0SDc8yuSEFQ4YTm5LjprOw&q=dragon ball z&limit=10
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10}`)
-      .subscribe((resp: any) => {
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10}`)
+      .subscribe((resp) => {
         console.log(resp.data);
         this.results = resp.data;
       });
